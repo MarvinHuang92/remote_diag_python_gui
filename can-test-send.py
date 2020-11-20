10c28afd1a7b94134955d8871bbdc9852b7ca341
MarvinHuang92 <marvinhuang@qq.com>
2020-11-20 10:29:44 +0800

    add can-test scripts

---
+++ can-test-send.py
@@ -0,0 +1,17 @@
+import time
+import can
+ 
+bustype = 'socketcan_native'
+channel = 'can0'
+ 
+def producer(id):
+    """:param id: Spam the bus with messages including the data id."""
+    bus = can.interface.Bus(channel=channel, bustype=bustype)
+    while True:
+        for i in range(10):
+            msg = can.Message(arbitration_id=0xc0ffee, data=[id, i, 0, 1, 3, 1, 4, 1], extended_id=False)
+            bus.send(msg)
+        # Issue #3: Need to keep running to ensure the writing threads stay alive. ?
+        time.sleep(3)
+ 
+producer(10)
